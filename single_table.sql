83e83e30594538fe6da4b7b48373ffe4ba0ba26f
Sachin Chandy <sachinmchandy@gmail.com>
2018-02-08 10:58:06 +0000

    single table DDL and DML

--- single_table.sql
+++ single_table.sql
@@ -96,3 +96,102 @@ SELECT item, menu_price, ingredient_cost, round(prep_time * (13.0/60), 2) AS lab
   menu_price - ingredient_cost - round(prep_time * (13.0/60), 2) AS profit
   FROM menu_items
   ORDER BY profit DESC;
+
+
+/* films1.sql */
+
+INSERT INTO films VALUES
+  ('1984',	1956,	'scifi',	'Michael Anderson',	90),
+  ('Tinker Tailor Soldier Spy',	2011,	'espionage',	'Tomas Alfredson',	127),
+  ('The Birdcage',	1996,	'comedy',	'Mike Nichols',	118);
+
+
+/*
+return title and age in years of each movie, with the newest movie listed first
+*/
+
+SELECT title, date_part('year', current_date) - year AS age FROM films
+ORDER BY year DESC;
+
+/* or */
+
+SELECT title, extract(year from current_date) - year AS age FROM films
+ORDER BY age ASC;
+
+/*
+Write a SQL statement that will return the title and duration of each movie
+longer than two hours, with the longest movies first.
+*/
+
+SELECT title, duration FROM films
+WHERE duration > 120
+ORDER BY duration DESC;
+
+/*
+SQL statement that returns the title of the longest movie
+*/
+
+SELECT title FROM films
+ORDER BY duration DESC
+LIMIT 1;
+
+/*
+Write a SQL query to list the ten states with the most rows in the people table
+in descending order.
+*/
+
+SELECT state, count(state) AS count FROM people
+GROUP BY state ORDER BY count DESC LIMIT 10;
+
+/* also */
+
+SELECT state, COUNT(id) FROM people GROUP BY state ORDER BY count DESC LIMIT 10;
+
+
+/*
+Write a SQL query that lists each domain used in an email address in the
+people table and how many people in the database have an email address
+containing that domain. Domains should be listed with the most popular first.
+*/
+
+
+SELECT count(id), substring(email from '%@#"_+#"%' for '#') AS domain
+  FROM people
+  GROUP BY domain
+  ORDER BY count(id) DESC;
+
+/*
+  residents=# SELECT count(id), substring(email from '%@#"_+#"%' for '#') AS domain
+  residents-#   FROM people
+  residents-#   GROUP BY domain
+  residents-#   ORDER BY count(id) DESC;
+   count |     domain
+  -------+----------------
+    1036 | cuvox.de
+    1029 | gustr.com
+    1024 | rhyta.com
+    1018 | superrito.com
+    1006 | armyspy.com
+    1000 | jourrapide.com
+     998 | teleworm.us
+     966 | dayrep.com
+     965 | einrot.com
+     958 | fleckens.hu
+  (10 rows)
+
+  residents=#
+*/
+
+/*
+Also this:
+*/
+SELECT substr(email, strpos(email, '@') + 1) as domain, COUNT(id) FROM people GROUP BY domain ORDER BY count DESC;
+
+
+/*
+Write a SQL statement that will update the given_name values to be all uppercase
+for all users with an email address that contains teleworm.us.
+*/
+UPDATE people
+SET given_name = upper(given_name)
+WHERE email LIKE '%teleworm.us'
