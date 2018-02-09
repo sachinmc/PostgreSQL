63845003afaf3c58ab3de7fb6fbef5eb967121a2
Sachin Chandy <sachinmchandy@gmail.com>
2018-02-09 23:44:31 +0000

    single table DDL and DML

--- single_table.sql
+++ single_table.sql
@@ -194,4 +194,185 @@ for all users with an email address that contains teleworm.us.
 */
 UPDATE people
 SET given_name = upper(given_name)
-WHERE email LIKE '%teleworm.us'
+WHERE email LIKE '%teleworm.us';
+
+
+/*
+Write the SQL statement to create a table called temperatures to hold the following data:
+
+date	low	high
+2016-03-01	34	43
+2016-03-02	32	44
+2016-03-03	31	47
+2016-03-04	33	42
+2016-03-05	39	46
+2016-03-06	32	43
+2016-03-07	29	32
+2016-03-08	23	31
+2016-03-09	17	28
+Keep in mind that all rows in the table should always contain all three values.
+*/
+
+CREATE TABLE temperatures (
+  date date NOT NULL,
+  low int NOT NULL,
+  high int NOT NULL
+);
+
+INSERT INTO temperatures VALUES
+  ('2016-03-01',	34,	43),
+  ('2016-03-02',	32,	44),
+  ('2016-03-03',	31,	47),
+  ('2016-03-04',	33,	42),
+  ('2016-03-05',	39,	46),
+  ('2016-03-06',	32,	43),
+  ('2016-03-07',	29,	32),
+  ('2016-03-08',	23,	31),
+  ('2016-03-09',	17,	28);
+
+/*
+Write a SQL statement to determine the average (mean) temperature
+for the days from March 2, 2016 through March 8, 2016.
+*/
+
+SELECT (high + low)/2 AS average, date FROM temperatures
+  WHERE date >= '2016-03-02' AND date <= '2016-03-08';
+
+/* or */
+
+SELECT date, (high + low) / 2 as average
+  FROM temperatures
+ WHERE date BETWEEN '2016-03-02' AND '2016-03-08';
+
+/*
+Each day, it rains one millimeter for every degree the average temperature goes
+above 35. Write a SQL statement to update the data in the table temperatures to
+reflect this.
+*/
+
+UPDATE temperatures
+SET rainfall = (high + low)/2 - 35
+WHERE (high + low)/2 > 35;
+
+/*
+A decision has been made to store rainfall data in inches. Write the SQL
+statements required to modify the rainfall column to reflect these new
+requirements.
+(25.4 mm make up an inch)
+*/
+
+ALTER TABLE temperatures
+ALTER COLUMN rainfall TYPE numeric(4,3);
+
+UPDATE temperatures
+SET rainfall = trunc(rainfall/25.4, 3);
+
+/* or */
+
+ALTER TABLE temperatures ALTER COLUMN rainfall TYPE numeric(6,3);
+UPDATE temperatures SET rainfall = rainfall * 0.039;
+
+/*
+The idea behind this dump method is to generate a file with SQL commands that,
+when fed back to the server, will recreate the database in the same state as it
+was at the time of the dump. PostgreSQL provides the utility program pg_dump for
+this purpose. The basic usage of this command is:
+*/
+
+pg_dump dbname > outfile
+
+/* from terminal */
+$ pg_dump -d sql-course -t weather --inserts > dump.sql
+
+
+/*
+Add a constraint to films that requires all rows to have a value for director
+that is at least 3 characters long and contains at least one space character ()
+*/
+
+ALTER TABLE films
+ADD CHECK (length(director) >= 3 AND director LIKE '% %');
+
+/* or */
+
+ALTER TABLE films ADD CONSTRAINT director_name
+CHECK (length(director) >= 3 AND position(' ' in director) > 0);
+
+
+
+/*
+Write SQL statements that will insert the following films into the database:
+
+title	year	genre	director	duration
+Wayne's World	1992	comedy	Penelope Spheeris	95
+Bourne Identity	2002	espionage	Doug Liman	118
+*/
+
+INSERT INTO films VALUES
+('Wayne''s World', 1992, 'comedy', 'Penelope Spheeris', 95),
+('Bourne Identity', 2002, 'espionage', 'Doug Liman', 118);
+
+
+/*
+Write a SQL query that lists all genres for which there is a movie in
+the films table.
+*/
+
+SELECT DISTINCT genre FROM films;
+
+/* or */
+
+SELECT genre FROM films
+GROUP BY genre;
+
+
+/* average time from all films, rounded value */
+SELECT round(avg(duration)) AS average FROM films;
+
+
+/*
+Write a SQL query that determines the average duration for each genre in
+the films table, rounded to the nearest minute.
+*/
+
+SELECT genre, round(avg(duration)) AS average_duration FROM films
+GROUP BY genre;
+
+/*
+Write a SQL query that determines the average duration of movies for each
+decade represented in the films table, rounded to the nearest minute and
+listed in chronological order.
+*/
+
+SELECT year/10 * 10 AS decade, round(avg(duration))
+FROM films GROUP BY decade ORDER BY decade;
+
+
+sql_course=# SELECT * FROM films
+WHERE director LIKE 'John%';
+/*
+ id |   title    | year | genre  |        director         | duration
+----+------------+------+--------+-------------------------+----------
+  1 | Die Hard   | 1988 | action | John McTiernan          |      132
+ 12 | Home Alone | 1990 | comedy | John Wilden Hughes, Jr. |      102
+ 13 | Hairspray  | 1988 | comedy | John Waters             |       92
+(3 rows)
+*/
+
+
+sql_course=# SELECT genre, count(id) FROM films
+GROUP BY genre ORDER BY count DESC;
+
+/*
+   genre   | count
+-----------+-------
+ scifi     |     5
+ comedy    |     4
+ drama     |     2
+ espionage |     2
+ crime     |     1
+ horror    |     1
+ thriller  |     1
+ action    |     1
+(8 rows)
+*/
