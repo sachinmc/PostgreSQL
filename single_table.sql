404f2016446a027a6e4b5ba126394f9f7bf7d3da
Sachin Chandy <sachinmchandy@gmail.com>
2018-02-11 22:17:03 +0000

    single table updates

--- single_table.sql
+++ single_table.sql
@@ -376,3 +376,53 @@ GROUP BY genre ORDER BY count DESC;
  action    |     1
 (8 rows)
 */
+
+
+/*  SQL query that will return the following data: */
+
+sql_course=# SELECT year/10 * 10 AS decade, genre, string_agg(title, ', ') AS films
+sql_course-# FROM films
+sql_course-# GROUP BY decade, genre
+sql_course-# ORDER BY decade;
+
+/*
+ decade |   genre   |                  films
+--------+-----------+------------------------------------------
+   1940 | drama     | Casablanca
+   1950 | drama     | 12 Angry Men
+   1950 | scifi     | 1984
+   1970 | crime     | The Godfather
+   1970 | thriller  | The Conversation
+   1980 | action    | Die Hard
+   1980 | comedy    | Hairspray
+   1990 | comedy    | Home Alone, The Birdcage, Wayne's World
+   1990 | scifi     | Godzilla
+   2000 | espionage | Bourne Identity
+   2000 | horror    | 28 Days Later
+   2010 | espionage | Tinker Tailor Soldier Spy
+   2010 | scifi     | Midnight Special, Interstellar, Godzilla
+(13 rows)
+
+sql_course=#
+*/
+
+sql_course=# SELECT genre, sum(duration) AS total_duration
+sql_course-# FROM FILMS
+sql_course-# GROUP BY genre
+sql_course-# ORDER BY total_duration;
+
+/*
+   genre   | total_duration
+-----------+----------------
+ thriller  |            113
+ horror    |            113
+ action    |            132
+ crime     |            175
+ drama     |            198
+ espionage |            245
+ comedy    |            407
+ scifi     |            632
+(8 rows)
+
+sql_course=#
+*/
